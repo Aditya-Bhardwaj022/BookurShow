6bfeb5419bcf737cd6cb3022726496943f855f5b
dityaislost <bhardwajaditya0203@gmail.com>
2026-03-27 02:04:07 +0530

    updated showservice

--- src/main/java/com/aditya/bms/repository/MovieRepository.java
+++ src/main/java/com/aditya/bms/repository/MovieRepository.java
@@ -15,7 +15,7 @@ public interface MovieRepository extends JpaRepository<Movie,Long>
 
     List<Movie> findByGenre(String genre);
 
-    List<Movie>findByTitleContainingString title);
+    List<Movie>findByTitleContaining(String title);
 
 
 }
